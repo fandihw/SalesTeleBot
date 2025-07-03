8053e1d9ce1c534059806126e72a90afbbbb30fc
fandihw <fandihardianwicaksana13@gmail.com>
2025-07-03 16:57:34 +0700

    push

--- services/drive.py
+++ services/drive.py
@@ -1,21 +1,41 @@
 import os
+import json
+from dotenv import load_dotenv
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-from config import DRIVE_FOLDER_ID
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 
-def _authorize():
+load_dotenv()
+
+DRIVE_FOLDER_ID = os.getenv("DRIVE_FOLDER_ID")
+
+# Autentikasi dari .env (bukan file service_account.json lagi)
+def authorize_drive():
+    json_str = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
+    info = json.loads(json_str)
+
+    credentials = service_account.Credentials.from_service_account_info(
+        info,
+        scopes=["https://www.googleapis.com/auth/drive"]
+    )
+
     gauth = GoogleAuth()
-    gauth.LocalWebserverAuth()
+    gauth.auth_method = 'service'
+    gauth.credentials = credentials
     return GoogleDrive(gauth)
 
-_drive = _authorize()
+drive = authorize_drive()
 
-def upload_photo(file_path: str) -> str:
-    file_meta = {
-        "title": os.path.basename(file_path),
-        "parents": [{"id": DRIVE_FOLDER_ID}],
-    }
-    gfile = _drive.CreateFile(file_meta)
+def upload_photo(file_path):
+    from os.path import basename
+
+    gfile = drive.CreateFile({
+        'title': basename(file_path),
+        'parents': [{'id': DRIVE_FOLDER_ID}]
+    })
     gfile.SetContentFile(file_path)
     gfile.Upload()
-    return f"https://drive.google.com/uc?id={gfile['id']}"
+    return gfile['alternateLink']
